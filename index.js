ef7740714db7248a21ad1034949ce46653b467d3
Sharon Nrangi <sharonnarangi@gmail.com>
2024-09-22 16:25:40 +0300

    Zomato Landing JavaScript

---
+++ index.js
@@ -0,0 +1,22 @@
+import { city } from "./city.js";
+
+
+
+
+let localitiesContainerEl = document.querySelector(".localities-container")
+
+
+let htmlLocalitiesList = city.map((el)=>{
+    return `
+    <div class="localities-list">
+      <div class="localities-card">
+        <h3>${el.name}</h3>
+        <p>${el.place}</p>
+      </div>
+      <i class="fas fa-angle-right"></i>
+    </div>
+  `
+}).join("");
+
+localitiesContainerEl.innerHTML = htmlLocalitiesList
+
