4e8a4be37000bf0a4bcdd2d148969b6b11143142
Sharon Nrangi <sharonnarangi@gmail.com>
2024-09-22 14:59:53 +0300

    Zomato Landing Page JS

---
+++ city.js
@@ -0,0 +1,49 @@
+export const city =[ {
+    name: "Connaught Place",
+    place: 289,
+
+},
+
+{
+    name: "Sector 29",
+    place: 149,
+
+},
+{
+    name: "Sector 18, Noida",
+    place: 236,
+
+},
+{
+    name: "Rajouri Garden",
+    place: 400,
+
+},
+{
+    name: "Saket",
+    place: 357,
+
+},
+{
+    name: "DLF Cyber City",
+    place: 190,
+
+},
+{
+    name: "Golf Course Road",
+    place: 168,
+
+},
+{
+    name: "DLF Phase 4",
+    place: 239,
+
+},
+{
+    name: "see more",
+    icon: "fa-solid fa-angle-right"
+    
+
+}
+
+]
